fc70a2fc3ce60bc85890e21d16b25a9538acf064
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-09-01 16:23:32 -1000

    Keep Hex-only tooling outside MWO seam

--- mix.exs
+++ mix.exs
@@ -1,5 +1,3 @@
-if bootstrap = System.get_env("MIX_WORKSPACE_OPS_BOOTSTRAP"), do: Code.require_file(bootstrap)
-
 defmodule Chassis.Workspace.MixProject do
   use Mix.Project
 
@@ -33,7 +31,7 @@ defmodule Chassis.Workspace.MixProject do
 
   defp deps do
     [
-      workspace_dep({:blitz, "~> 0.3.0", runtime: false}),
+      {:blitz, "~> 0.3.0", runtime: false},
       {:chassis_doctor, path: "bootstrap/chassis_doctor"},
       {:chassis_evolution_conformance, path: "proof/chassis_evolution_conformance"},
       {:chassis_hardware_guard, path: "model/chassis_hardware_guard"},
@@ -43,17 +41,11 @@ defmodule Chassis.Workspace.MixProject do
       {:chassis_stack_manager, path: "manager/chassis_stack_manager"},
       {:chassis_tensor_reload, path: "model/chassis_tensor_reload"},
       {:chassis_weight_materializer, path: "model/chassis_weight_materializer"},
-      workspace_dep({:weld, "~> 0.8.4", only: [:dev, :test], runtime: false}),
+      {:weld, "~> 0.8.4", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7.19", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4.7", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.40.3", only: :dev, runtime: false}
     ]
-  end
-
-  defp workspace_dep(committed) do
-    if function_exported?(MixWorkspaceOpsBootstrap, :dep, 2),
-      do: apply(MixWorkspaceOpsBootstrap, :dep, [committed, __DIR__]),
-      else: committed
   end
 
   def cli do
